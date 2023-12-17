fcfe67a9944b5b060e2014439895fbaf1bf5dbce
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2023-12-17 03:21:14 +0000

    continuing to progress

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -13,6 +13,18 @@ const getState = ({ getStore, getActions, setStore }) => {
 					email: "janesmith@example.com",
 					phone: "9876543210",
 					address: "456 Elm St"
+				},
+				{
+					fullName: "Michael Johnson",
+					email: "michaeljohnson@example.com",
+					phone: "5555555555",
+					address: "789 Oak St"
+				},
+				{
+					fullName: "Emily Davis",
+					email: "emilydavis@example.com",
+					phone: "9999999999",
+					address: "321 Pine St"
 				}
 			],
 			
