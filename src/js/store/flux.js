8fb96996be4d0e9a9d8a9ee34b652ef03af426c9
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2023-12-17 23:59:33 +0000

    made some more changes

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -30,7 +30,59 @@ const getState = ({ getStore, getActions, setStore }) => {
 			
 		},
 		actions: {
-				
+				getContactList: async () => {
+					const store = getStore();
+					const response = await fetch("https://playground.4geeks.com/apis/fake/contact/agenda/Scotts_blahblah");
+					const data = await response.json();
+					setStore({ contactList: [...store.contactList, data] });
+				},
+				addContact: async (fullName, email, phone,address) => {
+					const store = getStore();
+					const response = await fetch("https://playground.4geeks.com/apis/fake/contact/",{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json"
+						},
+						body: JSON.stringify({
+							full_name: fullName,
+							email: email,
+							agenda_slug: "Scotts_blahblah",
+							address: address,
+							phone: phone
+						})
+					});
+					const data = await response.json();
+					setStore({ contactList: [...store.contactList, data] });
+				},
+				updateContact: async (fullName, email, phone,address,contactId) => {
+					const store = getStore();
+					const response = await fetch("https://playground.4geeks.com/apis/fake/contact/"+contactId,{
+						method: "PUT",
+						headers: {
+							"Content-Type": "application/json"
+						},
+						body: JSON.stringify({
+							full_name: fullName,
+							email: email,
+							agenda_slug: "Scotts_blahblah",
+							address: address,
+							phone: phone
+						})
+					});
+					const data = await response.json();
+					setStore({ contactList: [...store.contactList, data] });
+				},
+				deleteContact: async (contactId) => {
+					const store = getStore();
+					const response = await fetch("https://playground.4geeks.com/apis/fake/contact/"+contactId,{
+						method: "DELETE",
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+					const data = await response.json();
+					setStore({ contactList: store.contactList.filter((contact) => contact.id !== contactId) });
+				}
 		}
 	};
 };
